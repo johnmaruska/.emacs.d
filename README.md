7f3b167422aefa54bc2c7be4d01ee9964b9532dc
John Maruska <john.maruska@rate.com>
2021-01-07 12:06:32 -0600

    Collapse appearance defs to all use 135 height JetBrains Mono font

--- README.md
+++ README.md
@@ -10,3 +10,21 @@
 - get eshell to color git diff without breaking magit
   (`git config --global color.ui always` breaks it)
 - info-bar at top instead of bottom of frame?
+
+
+## Requirements
+
+Different machines may have different requirements buried beneath some
+conditionals. I'm just going to list these without specifying because
+I hope to approach a consistent setup across machines.
+
+Fonts:
+- [JetBrains Mono font](https://www.jetbrains.com/lp/mono/)
+
+CLI tools:
+- ack and/or ag
+- git
+
+Directories/Files:
+- ~/dev
+- ~/dev/work/troubleshooting/journal.md
--- appearance.el
+++ appearance.el
@@ -5,9 +5,6 @@
 
 ;;; Commentary:
 
-;; Probably I'll have to duplicate a lot of this stuff across different
-;; platforms to use the same "feel" configuration everywhere...
-
 ;;; Code:
 
 ;;; Background Appearance
@@ -23,44 +20,15 @@
   ;; TODO: Look up a theme with a more beige background
   (load-theme 'sanityinc-tomorrow-day t))
 
-(defun vingtor-appearance ()
-  "Change the appearance of Emacs to preferred settings for Desktop Windows."
+(defun default-appearance ()
+  "Change the appearance of Emacs to preferred default settings."
   (interactive)
-  (set-face-attribute 'default nil
-                      :height 131
-                      :font "Consolas")
-  (dark-background))
-
-(defun convertible-appearance ()
-  "Change the appearance of Emacs to preferred settings for convertible Windows."
-  (interactive)
-  (set-face-attribute 'default nil
-                      :height 130
-                      :font "Source Code Pro")
-  (dark-background))
-
-(defun macbook-appearance ()
-  "Change the appearance of Emacs to preferred settings for floating Macbook."
-  (interactive)
+  (dark-background)
   (set-face-attribute 'default nil
                       :height 135
                       :weight 'medium
                       :width 'ultra-expanded
-                      :font "Andale Mono")
-  (dark-background))
-
-(defun default-default-appearance ()
-  (dark-background))
-
-(require 'computers "~/.emacs.d/computers.el")
-(defun default-appearance ()
-  "Change the appearance of Emacs to preferred default settings."
-  (interactive)
-  (cond
-   ((gr-macbook?)  (macbook-appearance))
-   ((vingtor?)     (vingtor-appearance))
-   ((convertible?) (convertible-appearance))
-   (t              (default-default-appearance))))
+                      :font "JetBrains Mono"))
 
 (defun antman-uses-emacs ()
   "Make font tiny."
