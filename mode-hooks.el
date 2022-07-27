8a1f34e6b0218aabf509cef3e2ca97fe3c04aa77
John Maruska <jackmaruska@gmail.com>
2022-07-27 09:52:51 -0500

    Don't aggressive-indent JSON because it keeps coming out wonky

--- mode-hooks.el
+++ mode-hooks.el
@@ -7,7 +7,9 @@
 
 (use-package aggressive-indent
   :ensure t
-  :config (global-aggressive-indent-mode 1))
+  :config
+  (global-aggressive-indent-mode 1)
+  (add-to-list 'aggressive-indent-excluded-modes 'json-mode))
 
 (use-package autorevert :delight auto-revert-mode)
 
